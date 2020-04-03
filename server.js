18641577547f392884043f19dbc3ebc833a1e93d
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-02 20:47:46 -0600

    bug fixes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/public'));
