85fc1e1e99c691e07327972b24b925fe32aed25c
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-07 12:22:04 -0600

    bug fixes

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(express.static(__dirname + '/public'));
-app.use('/', require('./controllers/burger_controller'));
+app.get(express.static(__dirname + '/public'));
+app.get('/', require('./controllers/burger_controller'));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
